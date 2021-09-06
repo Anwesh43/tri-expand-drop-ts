e6b5d5a8d293aa8ba25e3cec2e29f61803e98bce
Anwesh43 <anweshthecool0@gmail.com>
2021-09-06 14:12:09 +0530

    adding TEDNode class

--- index.ts
+++ index.ts
@@ -140,3 +140,45 @@ class Animator {
         }
     }
 }
+
+class TEDNode {
+
+    prev : TEDNode 
+    next : TEDNode 
+    state : State = new State()
+
+    constructor(private i : number) {
+        this.addNeighbor()
+    }
+
+    addNeighbor() {
+        if (this.i < colors.length - 1) {
+            this.next = new TEDNode(this.i + 1)
+            this.next.prev = this 
+        }
+    }
+
+    draw(context : CanvasRenderingContext2D) {
+        DrawingUtil.drawTEDNode(context, this.i, this.state.scale)
+    }
+
+    update(cb : Function) {
+        this.state.update(cb)
+    }
+    
+    startUpdating(cb : Function) {
+        this.state.startUpdating(cb)
+    }
+
+    getNext(dir : number, cb : Function) : TEDNode {
+        var curr : TEDNode = this.prev 
+        if (dir == 1) {
+            curr = this.next 
+        }
+        if (curr) {
+            return curr 
+        }
+        cb()
+        return this 
+    }
+}
