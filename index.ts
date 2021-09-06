8cb52c6f0d3a487bc16887c88b21e923bad2ada1
Anwesh43 <anweshthecool0@gmail.com>
2021-09-06 14:14:44 +0530

    adding TriExpanDrop class

--- index.ts
+++ index.ts
@@ -182,3 +182,26 @@ class TEDNode {
         return this 
     }
 }
+
+class TriExpandDrop {
+
+    curr : TEDNode = new TEDNode(0)
+    dir : number = 1
+
+    draw(context : CanvasRenderingContext2D) {
+        this.curr.draw(context)
+    }
+
+    update(cb : Function) {
+        this.curr.update(() => {
+            this.curr = this.curr.getNext(this.dir, () => {
+                this.dir *= -1
+            })
+            cb()
+        })
+    }
+
+    startUpdating(cb : Function) {
+        this.curr.startUpdating(cb)
+    }
+}
